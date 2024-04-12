e6c3b9110ff9e807d3d267b3220e91d8aee58137
Dixxtrap <djiga2015@gmail.com>
2024-04-12 13:37:32 +0000

    add port for preview

--- back-office/vite.config.ts
+++ back-office/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory
+    assetsDir: 'assets', // Assets directory
+    // Other build options
+  },
 server:{
   port:3001,
   proxy:{
