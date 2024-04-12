8d7f4583c3f6a20d54d989e46a5c45de93211508
Dixxtrap <djiga2015@gmail.com>
2024-04-12 16:03:05 +0000

    dacc

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Output directory
+    assetsDir: 'src/assets', // Assets directory
+    // Other build options
+  },
   server: {
     port: 3002,
     proxy: {
