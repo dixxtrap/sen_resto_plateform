9e3b3d24502f8313c48770c6af1868b29a3d0211
Dixxtrap <djiga2015@gmail.com>
2024-04-12 16:14:51 +0000

    daxx

--- client/src/interfaces/components/header.tsx
+++ client/src/interfaces/components/header.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
-import logo from "../../assets/react.svg";
+import logo from "/assets/react.svg";
 import { constant, links } from "../../utils/constant";
 import { Link, NavLink } from "react-router-dom";
 import clsx from "clsx";
--- client/src/interfaces/components/logo.tsx
+++ client/src/interfaces/components/logo.tsx
@@ -1,4 +1,4 @@
-import logo from "../../../src/assets/react.svg"
+import logo from "/assets/react.svg"
 
 export const Logo=()=>{
         return <img src={logo}/>
