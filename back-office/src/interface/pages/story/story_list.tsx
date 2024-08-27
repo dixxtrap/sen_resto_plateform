9c04808b2f08a666a4e4de48ac4d9724c7f7ba4c
Charles-Ababcar <ababacarcharles@gmail.com>
2024-08-27 17:25:43 +0000

    story

--- back-office/src/interface/pages/story/story_list.tsx
+++ back-office/src/interface/pages/story/story_list.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal} from "@mantine/core";
+import { Badge, Button, Card, Group, Modal,Text} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 
 export const StoreList = () => {
@@ -6,7 +6,20 @@ export const StoreList = () => {
   return (
     <div>
       <Modal opened={opened} onClose={close}>Bonjour </Modal>
-      <Button onClick={open}>Ajouter</Button>
+          <Button onClick={open}>Ajouter</Button>
+          
+          <Card shadow="sm" padding="lg" radius="md" withBorder>
+    
+
+      <Text size="sm" c="dimmed">
+        With Fjord Tours you can explore more of the magical fjord landscapes with tours and
+        activities on and around the fjords of Norway
+      </Text>
+
+      <Button color="blue" fullWidth mt="md" radius="md">
+        Book classic tour now
+      </Button>
+    </Card>
     </div>
   );
 };
