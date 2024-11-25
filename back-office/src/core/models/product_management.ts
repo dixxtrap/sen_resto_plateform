c581a9091b0a4127b9631fd37fef4fa7232af57f
dixxtrap <djiga2015@gmail.com>
2024-11-25 13:46:53 +0000

    daxx

--- back-office/src/core/models/product_management.ts
+++ back-office/src/core/models/product_management.ts
@@ -1,7 +1,6 @@
 import { CompanyDto } from "./company.dto";
 import { CreationDetailDto } from "./creation_details.dto";
 import {  ProductDto } from "./product";
-import { ProductManagementDayDto } from "./product_management_day";
 export class ProductManagementDto{
       id?:number;
         productId?:number;
@@ -10,5 +9,4 @@ export class ProductManagementDto{
         partnerId?:number;
         isActive?:boolean;
         details?:CreationDetailDto;
-        productManagementDay?:ProductManagementDayDto[]
 }
