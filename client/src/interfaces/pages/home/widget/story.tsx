a851a7c08c7ee4ab67a1034458401106f99c2a4f
dixxtrap <djiga2015@gmail.com>
2024-12-04 23:46:31 +0000

    daxx

--- client/src/interfaces/pages/home/widget/story.tsx
+++ client/src/interfaces/pages/home/widget/story.tsx
@@ -76,7 +76,7 @@ export const Story = () => {
             h={{ base: '60px', md: rem(200) }}
           
            
-            className="bg-transparent"
+            className="bg-white md:bg-transparent"
             slideSize={{ base: '60px', md: rem(250) }}
             // classNames={{slide:"size-[200px]"}}
             slideGap="lg"
